6f84e56b7afb38ad3f748ed98d912c0c2f45c834
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2020-10-16 14:25:51 -0300

    Criação da camada Service

---
+++ TjspAutomacao/Service/Planilha.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TjspAutomacao.Service
+{
+    class Planilha
+    {
+        private readonly int NUM_MINIMO_LINHAS = 1;
+        private readonly int NUM_MINIMO_COLUNAS = 1;
+        private readonly int NUM_CORRETO_COLUNAS = 3;
+
+        private DataTable dtProcessos;
+        public Planilha()
+        {
+            this.dtProcessos = new DataTable();
+            InicializaDataTable();
+        }
+
+        private void InicializaDataTable()
+        {
+            if(dtProcessos.Columns.Count < NUM_MINIMO_COLUNAS)
+            {
+                dtProcessos.Columns.Add(new DataColumn("Número do Processo"));
+                dtProcessos.Columns.Add(new DataColumn("Foro"));
+                dtProcessos.Columns.Add(new DataColumn("Competência"));
+            }            
+        }
+
+        public DataTable CarregaDataGridView(string caminhoArquivo)
+        {
+            if (string.IsNullOrEmpty(caminhoArquivo))
+            {
+                MessageBox.Show("O caminho do arquivo é nulo ou vazio");
+            }
+            else
+            {
+                //Verifico se há algum dado na datagridview, se tiver limpa o datagrid
+                if (dtProcessos.Rows.Count > NUM_MINIMO_LINHAS)
+                {
+                    dtProcessos.Rows.Clear();
+                }
+
+                String[] linha = System.IO.File.ReadAllLines(caminhoArquivo);
+
+                String[] cabecalho = linha[0].Split(Convert.ToChar(";"));                
+
+                if(cabecalho.Length < NUM_CORRETO_COLUNAS || cabecalho.Length > NUM_CORRETO_COLUNAS)
+                {
+                    MessageBox.Show("Planilha com número incorreto de colunas, a quantidade correta é :" +  NUM_CORRETO_COLUNAS.ToString());
+                }
+                else
+                {                    
+                    for (int posLinha = 1; posLinha < linha.Length; posLinha++)
+                    {
+                        String[] dados = linha[posLinha].Split(Convert.ToChar(";"));
+                        dtProcessos.Rows.Add(dados);
+                    }
+                }                           
+            }
+            return dtProcessos;
+        }
+    }
+}
---
+++ TjspAutomacao/Service/Protocolo.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TjspAutomacao.Classe
+{
+    class Protocolo
+    {
+        private readonly string URL_ESAJ = "https://esaj.tjsp.jus.br/sajcas/login?service=https%3A%2F%2Fesaj.tjsp.jus.br%2Fpetpg%2Fj_spring_cas_security_check";
+        private IWebDriver navegador;
+
+        public Protocolo()
+        {
+            this.navegador = new ChromeDriver();
+        }
+
+        public void AbreTJSP()
+        {
+            navegador.Navigate().GoToUrl(URL_ESAJ);                        
+        }
+
+        public void Login(string cpf, string senha)
+        {
+            if(string.IsNullOrEmpty(cpf) || string.IsNullOrEmpty(senha))
+            {
+                MessageBox.Show("Login ou senha é nulo ou está em branco");
+            }
+            else
+            {
+                navegador.FindElement(By.Id("usernameForm")).SendKeys(cpf);
+                navegador.FindElement(By.Id("passwordForm")).SendKeys(senha);
+            }            
+        }
+    }
+}
