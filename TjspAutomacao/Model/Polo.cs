0300eddeb744f9a34224a8e3e744bc58c8ce55fc
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2020-10-16 14:27:06 -0300

    Criação da camada Model e classes Polo e Processo

---
+++ TjspAutomacao/Model/Polo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjspAutomacao.Model
+{
+    class Polo
+    {
+        public string Participacao { get; set; }
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public int Genero { get; set; }
+        public string RG { get; set; }
+        public string OrgaoEmissor { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Nacionalidade { get; set; }
+        public string Profissao { get; set; }
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public Polo()
+        {
+
+        }     
+
+    }
+}
