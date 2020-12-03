bf82facbbef2c2cea2bdc10fbb20c74894141bdc
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2020-12-03 16:03:09 -0300

    Add classes CertificadoXPath e DocumentoXPath

---
+++ TjspAutomacao/XPath/CertificadoXPath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjspAutomacao.XPath
+{
+    class CertificadoXPath
+    {
+        public static readonly string CONTAINER_CERTIFICADO_ASSINATURA = "//*[@id='containerCertificadoParaAssinatura']/div[2]/div/div/div[1]/span";
+        public static readonly string BOTAO_PROTOCOLAR = "botaoProtocolar";
+        public static readonly string BOTAO_PROTOCOLAR_SIM = "html/body/div[2]/div[2]/div/div[1]/button";
+
+    }
+}
---
+++ TjspAutomacao/XPath/DocumentoXPath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjspAutomacao.Model
+{
+    class DocumentoXPath
+    {        
+        public static string ElementoClicavel(int posicao)
+        {
+            return "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li[" + posicao.ToString() +"]/div/nav/div/ul/li[3]/span/div/div[1]/span/span[1]";
+        }
+
+        public static string InputTipoDocumento(int posicao)
+        {           
+            if (posicao == 1)
+                return "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li/div/nav/div/ul/li[3]/span/div/input[1]";
+            else
+                return "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li[" + posicao.ToString() +"]/div/nav/div/ul/li[3]/span/div/input[1]";
+        }        
+    }
+}
