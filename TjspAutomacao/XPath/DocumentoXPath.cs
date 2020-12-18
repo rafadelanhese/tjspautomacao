5521a221adc817eedff4ca04bbee34f7e1b04d92
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2020-12-18 16:22:42 -0300

    Add variável estática INPUT_DOCUMENTO

--- TjspAutomacao/XPath/DocumentoXPath.cs
+++ TjspAutomacao/XPath/DocumentoXPath.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TjspAutomacao.Model
-{
+{        
     class DocumentoXPath
-    {        
+    {
+        public static readonly string INPUT_DOCUMENTO = "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[2]/button/input";
         public static string ElementoClicavel(int posicao)
         {
-            return "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li[" + posicao.ToString() +"]/div/nav/div/ul/li[3]/span/div/div[1]/span/span[1]";
+            return "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li[" + posicao.ToString() +"]/div/nav/div/ul/li[3]/span/div/div[1]/span";
         }
 
         public static string InputTipoDocumento(int posicao)
