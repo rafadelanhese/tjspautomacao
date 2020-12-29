0a0550c14d6c3dd22570c29176790584d5299714
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2020-12-29 12:26:26 -0300

    Add métodos GetBotaoTipoDocumento e GetInputTipoDocumento, exclusão dos métodos anteriores

--- TjspAutomacao/XPath/DocumentoXPath.cs
+++ TjspAutomacao/XPath/DocumentoXPath.cs
@@ -9,17 +9,22 @@ namespace TjspAutomacao.Model
     class DocumentoXPath
     {
         public static readonly string INPUT_DOCUMENTO = "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[2]/button/input";
-        public static string ElementoClicavel(int posicao)
+        private string BotaoTipoDocumento = "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li[]/div/nav/div/ul/li[3]/span/div/div[1]/span";
+        private string InputTipoDocumento = "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li[]/div/nav/div/ul/li[3]/span/div/input[1]";
+        private readonly int posInsercaoString = 101;
+        public DocumentoXPath()
         {
-            return "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li[" + posicao.ToString() +"]/div/nav/div/ul/li[3]/span/div/div[1]/span";
+
         }
 
-        public static string InputTipoDocumento(int posicao)
-        {           
-            if (posicao == 1)
-                return "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li/div/nav/div/ul/li[3]/span/div/input[1]";
-            else
-                return "/html/body/span/main/div/div/div/form/div/div/div[1]/div/div/div/article/section[1]/footer/div/ul/li[" + posicao.ToString() +"]/div/nav/div/ul/li[3]/span/div/input[1]";
-        }        
+        public string GetBotaoTipoDocumento(int posicao)
+        {
+            return BotaoTipoDocumento.Insert(posInsercaoString, posicao.ToString());
+        }
+
+        public string GetInputTipoDocumento(int posicao)
+        {
+            return InputTipoDocumento.Insert(posInsercaoString, posicao.ToString());
+        }                 
     }
 }
