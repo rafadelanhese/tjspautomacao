193d9a31050a1e5fb58406d3eee0079f55ecf082
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2020-12-29 10:31:05 -0300

    Adição da classe DespesasProcessuaisXPath

---
+++ TjspAutomacao/XPath/DespesasProcessuaisXPath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjspAutomacao.XPath
+{
+    class DespesasProcessuaisXPath
+    {        
+        private readonly string BOTAO_DOCUMENTO = "//*[@id='secaoGuiaCustas']/div/div/button";
+        private int QtdeDare = 1;
+
+        public DespesasProcessuaisXPath()
+        {
+
+        }
+
+        public string GetBotaoDocumentoXPath()
+        {
+            if(QtdeDare == 1)
+            {
+                QtdeDare++;
+                return BOTAO_DOCUMENTO;
+            }
+
+            string auxQtdeDare = QtdeDare.ToString();
+            QtdeDare++;
+            return BOTAO_DOCUMENTO.Insert(30, "[" + auxQtdeDare + "]");
+        } 
+
+        public bool PrimeiraDare()
+        {
+            return QtdeDare == 1 ? true : false;
+        }
+    }
+}
